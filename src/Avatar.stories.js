9599d3c94465ef38c535eea4ab42fbe53ad3edeb
Kevin Klaus <kev.k1995@gmail.com>
2021-01-07 17:42:25 +0100

    prettier

---
+++ src/Avatar.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Avatar } from "./Avatar";
+
+// …
+
+// New story using controls
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  loading: false,
+  size: "tiny",
+  username: "Dominic Nguyen",
+  src: "https://avatars2.githubusercontent.com/u/263385",
+};
